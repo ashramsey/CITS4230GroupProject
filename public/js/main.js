d799a27f08f47de3ada427a7ac29c3e34bacbfbf
Ashley Ramsey <ashramsey@gmail.com>
2010-09-18 17:32:43 +0800

    update on javascript files and index page, and some referencing of img's

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,36 @@
-﻿//function for every page to open a new page of serach result
+﻿
+/* -------------------------------------------- START - Search Bar -------------------------*/
+//function for every page to open a new page of serach result
 function getsearch()
 {
 	var srtxt = document.getElementById("searchtxt").value;
 	window.open("http://www.google.com/search?forid=1&ie=utf-8&oe=UTF-8&hl=EN&q="+srtxt);
 }
+/* -------------------------------------------- END - Search Bar -------------------------*/
 
+/* -------------------------------------------- START - Index.html - Top Room List -------------------------*/
+numOfChatRooms = 5;
+// Auto generated chatRoom
+function chatRoomGen (){
+	var now = new Date();
+	var sec = now.getSeconds();
+	var rand1 = Math.random(sec); // to generate it everytime with different time.
+	var randUse = Math.floor(rand1 * (numOfChatRooms));
+	if(randUse == numOfChatRooms) randUse = 0; // to avoid getting more than array specified
+	return randUse;
+}
+
+function chatRoomDisplay(){
+	var num = chatRoomGen();
+	newid = "chatRoomsList"+num;
+	document.getElementById(newid).style.display = "block";		
+}
+/* -------------------------------------------- END - Index.html - Top Room List -------------------------*/
+
+/* -------------------------------------------- START - AboutUs.html -  -------------------------*/
 //function for aboutus.html to display text of special style
-function myfunc()
+
+function mystyle()
 {
 	var d=document.getElementById("talkfont");
 	var colours = new Array();
@@ -29,7 +53,34 @@ function myfunc()
 	colours[16] = 'yellow';
 
 	var rand = parseInt((Math.random()*(16))+0);
-	d.style.color = colours[rand];
+	d.style.color = colours[rand];    
 }
-	setInterval(myfunc,300);
+function myfunc()
+{
+    mystyle();
+    setInterval(mystyle,300);
+	
+}
+	
+/* -------------------------------------------- END - AboutUs.html -  -------------------------*/
+
+/* -------------------------------------------- START - Support Page - Topics Slider -------------------------*/
+$(document).ready(function() {
+    //Set default open/close settings
+  $('.acc_container').hide(); //Hide/close all containers
+  $('.acc_trigger:first').addClass('active').next().show(); //Add "active" class to first trigger, then show/open the immediate next container
+
+  //On Click
+  $('.acc_trigger').click(function(){
+  	if( $(this).next().is(':hidden') ) { //If immediate next container is closed...
+  		$('.acc_trigger').removeClass('active').next().slideUp(); //Remove all "active" state and slide up the immediate next container
+  		$(this).toggleClass('active').next().slideDown(); //Add "active" state to clicked trigger and slide down the immediate next container
+  	}
+  	return false; //Prevent the browser jump to the link anchor
+  });
+  
+});
+/* -------------------------------------------- END - Support Page - Topics Slider -------------------------*/
+
+
 
